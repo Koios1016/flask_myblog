b991c785953e4cd5d39a7cfb47b5756f42e138ea
Koios <koios1016@gmail.com>
2018-08-27 20:49:17 +0800

    init

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_login import LoginManager
+# 导入配置文件
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# 创建app应用,__name__是python预定义变量，被设置为使用本模块.
+app = Flask(__name__)
+# 添加配置信息
+app.config.from_object(Config)
+login = LoginManager(app)
+
+login.login_view = 'login'
+
+# 建立数据库关系
+db = SQLAlchemy(app)
+# 绑定app和数据库，以便进行操作
+migrate = Migrate(app, db)
+
+from app import routes, models
